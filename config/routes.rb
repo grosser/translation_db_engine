f57e73d0e9ff2bb810ae6af7f593fb278aa0388d
grosser <grosser.michael@gmail.com>
2010-08-04 10:47:22 +0200

    fix for rails 3 rc

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
-ActionController::Routing::Routes.draw do |map|
+router = if Rails::VERSION::MAJOR == '3'
+  Rails.application.routes
+else
+  ActionController::Routing::Routes
+end
+
+router.draw do |map|
   map.resources :translation_keys
 end
